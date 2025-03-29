d38337647db9959de9eac8c4bccc661665d18f6a
LT-Schmiddy <schmidalex1996@gmail.com>
2025-03-29 12:35:05 -0400

    singleplayer getters and setters created

--- apcpp-singleplayer-config.cpp
+++ apcpp-singleplayer-config.cpp
@@ -1,6 +1,5 @@
 #include "apcpp-singleplayer-config.h"
 
-
 SinglePlayerRandoConfig::SinglePlayerRandoConfig() {}
 SinglePlayerRandoConfig::~SinglePlayerRandoConfig() {}
 bool SinglePlayerRandoConfig::write_to_file(std::filesystem::path out_file) {
