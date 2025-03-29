bb408dca26a210a71e56a0df01f62bfd06a235cc
LT-Schmiddy <schmidalex1996@gmail.com>
2025-03-29 12:35:05 -0400

    singleplayer getters and setters created

--- apcpp-singleplayer-config.h
+++ apcpp-singleplayer-config.h
@@ -1,6 +1,7 @@
+#pragma once
+
 #include <string>
 #include <filesystem>
-#include "apcpp-glue.h"
 #include "Archipelago.h"
 
 
@@ -12,15 +13,15 @@ public:
     bool write_to_file(std::filesystem::path out_file);
 
     enum class Accessability {
-        FULL,
-        MINIMAL,
+        FULL = 0,
+        MINIMAL = 1,
     };
     Accessability accessability = Accessability::FULL;
 
     enum class LogicDifficulty {
-        EASY,
-        NORMAL,
-        NO_LOGIC
+        EASY = 0,
+        NORMAL = 1,
+        NO_LOGIC = 2
     };
     LogicDifficulty logicDifficulty = LogicDifficulty::NORMAL;
 
@@ -35,37 +36,38 @@ public:
     int startingHearts = 12;
     
     enum class StartingHeartsAreContainersOrPieces {
-        CONTAINERS,
-        PIECES
+        CONTAINERS = 0,
+        PIECES = 1
     };
     StartingHeartsAreContainersOrPieces startingHeartsAreContainersOrPieces = StartingHeartsAreContainersOrPieces::CONTAINERS;
 
     enum class ShuffleRegionalMaps {
-        VANILLA,
-        STARTING,
-        ANYWHERE
+        VANILLA = 0,
+        STARTING = 1,
+        ANYWHERE = 2
     };
     ShuffleRegionalMaps shuffleRegionalMaps = ShuffleRegionalMaps::VANILLA;
 
     enum class ShuffleBossRemains {
-        VANILLA,
-        ANYWHERE,
-        BOSSES
+        VANILLA = 0,
+        ANYWHERE = 1,
+        BOSSES = 2
     };
+    ShuffleBossRemains shuffleBossRemains = ShuffleBossRemains::VANILLA;
 
     bool shuffleSpiderHouseRewards = false;
 
     enum class SkullSanity {
-        VANILLA,
-        ANYTHING,
-        IGNORE
+        VANILLA = 0,
+        ANYTHING = 1,
+        IGNORE = 2
     };
     SkullSanity skullSanity = SkullSanity::VANILLA;
 
     enum class ShopSanity {
-        VANILLA,
-        ENABLED,
-        ADVANCED
+        VANILLA = 0,
+        ENABLED = 1,
+        ADVANCED = 2
     };
     ShopSanity shopSanity = ShopSanity::VANILLA;
 
@@ -73,24 +75,24 @@ public:
     bool cowSanity = false;
     bool shuffleCreatFairyRewards = false;
     bool fairySanity = false;
-    bool startWithConsumables = false;
+    bool start_with_consumables = false;
     bool permanentChateauRomani = true;
     bool startWithInvertedTime = true;
     bool recieveFilledWallets = true;
     
     enum class DamageMultiplier {
-        HALF,
-        NORMAL,
-        DOUBLE,
-        QUAD,
-        ONE_HOT_KO
+        HALF = 0,
+        NORMAL = 1,
+        DOUBLE = 2,
+        QUAD = 3,
+        ONE_HOT_KO = 4
     };
     DamageMultiplier damageMultiplier = DamageMultiplier::NORMAL;
 
     enum class DeathBehavior {
-        VANILLA,
-        FAST,
-        MOON_CRASH
+        VANILLA = 0,
+        FAST = 1,
+        MOON_CRASH = 2
     };
     DeathBehavior deathBehavior = DeathBehavior::VANILLA;
 };
