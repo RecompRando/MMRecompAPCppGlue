3378419c5f41810ff431c5ed3950100117aa2c96
LT-Schmiddy <schmidalex1996@gmail.com>
2025-03-28 23:09:17 -0400

    Basic config options available

---
+++ apcpp-config.h
@@ -0,0 +1,87 @@
+#include <string>
+
+
+class RandoConfig {
+public:
+    RandoConfig();
+    ~RandoConfig();
+
+    enum class Accessability {
+        FULL,
+        MINIMAL,
+    };
+    Accessability accessability = Accessability::FULL;
+
+    enum class LogicDifficulty {
+        EASY,
+        NORMAL,
+        NO_LOGIC
+    };
+    LogicDifficulty logicDifficulty = LogicDifficulty::NORMAL;
+
+    bool chestsMatchContents = true;
+    bool startSwordless = false;
+    bool startShieldless = false;
+    bool startWithSoaring = true;
+
+    enum class StartingHeartsAreContainersOrPieces {
+        CONTAINERS,
+        PIECES
+    };
+    StartingHeartsAreContainersOrPieces startingHeartsAreContainersOrPieces = StartingHeartsAreContainersOrPieces::CONTAINERS;
+
+    enum class ShuffleRegionalMaps {
+        VANILLA,
+        STARTING,
+        ANYWHERE
+    };
+    ShuffleRegionalMaps shuffleRegionalMaps = ShuffleRegionalMaps::VANILLA;
+
+    enum class ShuffleBossRemains {
+        VANILLA,
+        ANYWHERE,
+        BOSSES
+    };
+
+    bool shuffleSpiderHouseRewards = false;
+
+    enum class SkullSanity {
+        VANILLA,
+        ANYTHING,
+        IGNORE
+    };
+    SkullSanity skullSanity = SkullSanity::VANILLA;
+
+    enum class ShopSanity {
+        VANILLA,
+        ENABLED,
+        ADVANCED
+    };
+    ShopSanity shopSanity = ShopSanity::VANILLA;
+
+    bool scrubSanity = false;
+    bool cowSanity = false;
+    bool shuffleCreatFairyRewards = false;
+    bool fairySanity = false;
+    bool startWithConsumables = false;
+    bool permanentChateauRomani = true;
+    bool startWithInvertedTime = true;
+    bool recieveFilledWallets = true;
+    
+    enum class DamageMultiplier {
+        HALF,
+        NORMAL,
+        DOUBLE,
+        QUAD,
+        ONE_HOT_KO
+    };
+    DamageMultiplier damageMultiplier = DamageMultiplier::NORMAL;
+
+    enum class DeathBehavior {
+        VANILLA,
+        FAST,
+        MOON_CRASH
+    };
+    DeathBehavior deathBehavior = DeathBehavior::VANILLA;
+
+};
