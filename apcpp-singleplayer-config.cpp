7d9ddcf5c56343d4d8f88f7c8c6e8b903464829b
LT-Schmiddy <schmidalex1996@gmail.com>
2025-04-03 16:45:06 -0400

    Pivoting to modcode yaml generation

--- apcpp-singleplayer-config.cpp
+++ apcpp-singleplayer-config.cpp
@@ -1,10 +1,2 @@
 #include "apcpp-singleplayer-config.h"
 
-SinglePlayerRandoConfig::SinglePlayerRandoConfig() {}
-SinglePlayerRandoConfig::~SinglePlayerRandoConfig() {}
-bool SinglePlayerRandoConfig::write_to_file(std::filesystem::path out_file) {
-    return false;
-}
-    
-
-
