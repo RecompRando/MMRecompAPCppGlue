09ff414509989a45c17d3533da3203e0d1335d62
LT-Schmiddy <schmidalex1996@gmail.com>
2025-04-11 18:01:00 -0400

    adding u8string arg handler

--- apcpp-singleplayer-config-exports.cpp
+++ apcpp-singleplayer-config-exports.cpp
@@ -1,3 +1,11 @@
 #include <memory>
 #include "apcpp-glue.h"
 #include "apcpp-singleplayer-config.h"
+
+
+// extern "C" {
+    RECOMP_DLL_FUNC(rando_save_run_yaml) {
+        u32 slot = RECOMP_ARG(u32, 1);
+        std::string yaml_text = RECOMP_ARG_STR(2);
+    }
+// };
