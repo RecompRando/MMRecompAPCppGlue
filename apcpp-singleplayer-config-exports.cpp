54848c6cbc0d09685384bbebd3eda81a6fa9fe65
LT-Schmiddy <schmidalex1996@gmail.com>
2025-04-11 19:31:38 -0400

    adding append yaml function

--- apcpp-singleplayer-config-exports.cpp
+++ apcpp-singleplayer-config-exports.cpp
@@ -28,3 +28,23 @@ RECOMP_DLL_FUNC(rando_save_run_yaml) {
     }
     out.close();
 }
+
+RECOMP_DLL_FUNC(rando_append_run_yaml) {
+    std::u8string savepath_str = RECOMP_ARG_U8STR(0);
+    // std::u8string savepath_str = u8"testing 1 2 3\n";
+    u32 slot = RECOMP_ARG(u32, 1);
+    std::string yaml_text = RECOMP_ARG_STR(2);
+
+    std::cout << yaml_text;
+
+    fs::path savefile(savepath_str);
+    fs::path yamlfile = fs::path(savefile).parent_path().append(std::format("RANDO_SLOT_{}.yaml", slot));
+
+    std::ofstream out = std::ofstream(yamlfile, std::ios::app);
+    try {
+        out << yaml_text;
+    } catch (std::exception e){
+        std::cout << e.what();
+    }
+    out.close();
+}
